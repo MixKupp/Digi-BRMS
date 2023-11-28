3a565ee119de799c5ade517de3c917dd9662d319
Mixkupp <mixkup.thiwongsa@gmail.com>
2023-11-29 01:19:57 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,7 @@
-/**
- * main
- */
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello Project");
+        System.out.println("This is my first project");
     }
 }
